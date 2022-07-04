5f4ae04b5bf3ed284fceaa16e9da7fb1ca3bfe80
Lucas Andrade <lucasilva108@gmail.com>
2022-07-04 08:52:15 -0300

    feat: setting env vars

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,30 @@
+import { BullModule } from '@nestjs/bull';
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { MailerModule } from '@nestjs-modules/mailer';
+import { CreateUserController } from './create-user/create-user.controller';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot(),
+    BullModule.forRoot({
+      redis: {
+        host: process.env.REDIS_HOST,
+        port: Number(process.env.REDIS_PORT)
+      }
+    }),
+    MailerModule.forRoot({
+      transport: {
+        host: process.env.MAIL_HOST,
+        port: Number(process.env.MAIL_PORT),
+        auth: {
+          user: process.env.MAIL_USER,
+          pass: process.env.MAIL_PASS
+        }
+      }
+    })
+  ],
+  controllers: [CreateUserController],
+  providers: [],
 })
-export class AppModule {}
+export class AppModule { }
